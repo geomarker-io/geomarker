a80e15084423ccfad33427c10423fc476f59df2f
Cole Brokamp <cole.brokamp@gmail.com>
2026-01-07 20:18:40 -0500

    geomarker data dir and downloading

---
+++ R/geomarker_data.R
@@ -0,0 +1,100 @@
+#' Get the path to the geomarker data directory.
+#'
+#' By default, the user's data directory for the geomarker data package is used;
+#' see `?tools::R_user_dir`.
+#' Specify an alternative download location by setting the `R_USER_DATA_DIR`
+#' environment variable.
+#' @param subdir character (length one); optional subdirectory within the geomarker data folder
+#' @returns character string of path to directory
+#' @export
+#' @examples
+#' geomarker_data_path()
+#' geomarker_data_path("greenness")
+#' # use environment variable to change location
+#' withr::local_envvar(R_USER_DATA_DIR = tempdir())
+#' geomarker_data_path()
+#' withr::deferred_run()
+geomarker_data_dir <- function(subdir = character(0)) {
+  stopifnot(
+    "subdir must be a character vector" = inherits(subdir, "character"),
+    "subdir must not be longer than one" = length(subdir) <= 1,
+    "subdir must not be missing" = !is.na(subdir)
+  )
+  the_path <- tools::R_user_dir(package = "geomarker", which = "data")
+  if (length(subdir) > 0) {
+    the_path <- file.path(the_path, subdir)
+  }
+  if (!file.exists(the_path)) {
+    dir.create(the_path, recursive = TRUE)
+  }
+  the_path
+}
+
+# really only designed to be used with static URLs ending in filename; will break with query parameters??
+url_to_filename <- function(urls) {
+  url_dir_hash <- vapply(
+    dirname(urls),
+    digest::digest,
+    algo = "xxhash32",
+    serialize = FALSE,
+    FUN.VALUE = character(1)
+  )
+  f_name <- basename(urls)
+  filepath <- paste(url_dir_hash, f_name, sep = "--")
+  filepath
+}
+
+
+#' Download a file to the geomarker data directory
+#'
+#' The file at a URL is downloaded to the geomarker data directory
+#' (see `?geomarker_data_path()`) and named using a short hash of the
+#' non-filename portion of the URL in addition to a filename derived
+#' from the URL. By default, files that are already downloaded will not
+#' be downloaded again.
+#' @param url character (length one); URL of file to download
+#' @param overwrite logical; overwrite file if already downloaded?
+#' @param quiet logical; show download progress messages and print path to downloaded file?
+#' @param subdir character (length one); optional subdirectory within the geomarker data folder
+#' @returns character string of file path to downloaded file (by default, returned invisibly)
+#' @export
+geomarker_download_file <- function(
+  url,
+  overwrite = FALSE,
+  quiet = FALSE,
+  subdir = character(0)
+) {
+  stopifnot(
+    "url must be length one" = length(url) == 1,
+    "url must be character" = inherits(url, "character")
+  )
+  dest <- file.path(geomarker_data_dir(subdir = subdir), url_to_filename(url))
+  if (file.exists(dest) && !overwrite) {
+    if (quiet) {
+      return(invisible(dest))
+    }
+    return(dest)
+  }
+  tmp <- tempfile(pattern = "geomarker_dl_")
+  on.exit(unlink(tmp), add = TRUE)
+  if (requireNamespace("curl", quietly = TRUE)) {
+    curl::curl_download(url, destfile = tmp, quiet = quiet, mode = "wb")
+  } else {
+    message("install the curl package for a better downloading experience!")
+    utils::download.file(url, destfile = tmp, mode = "wb", quiet = quiet)
+  }
+  ok <- file.rename(tmp, dest)
+  if (!ok && !file.copy(tmp, dest, overwrite = TRUE)) {
+    stop(
+      "Failed to move downloaded file: ",
+      tmp,
+      " into destination: ",
+      dest,
+      call. = FALSE
+    )
+  }
+  if (quiet) {
+    return(invisible(dest))
+  }
+  dest
+}
---
+++ tests/testthat/test-geomarker_data.R
@@ -0,0 +1,50 @@
+test_that("geomarker_data_path works", {
+  geomarker_data_path() |>
+    expect_type("character") |>
+    expect_length(1) |>
+    grepl("geomarker", x = _) |>
+    expect_true()
+  geomarker_data_path("foofy") |>
+    expect_type("character") |>
+    expect_length(1) |>
+    grepl("foofy", x = _) |>
+    expect_true()
+})
+
+test_that("url_to_filename works", {
+  c(
+    "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MET/tmmx/tmmx_2025.nc",
+    "https://www2.census.gov/geo/tiger/TIGER2024/INTERNATIONALBOUNDARY/tl_2024_us_internationalboundary.zip",
+    "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/2025/12/hms_smoke20251225.zip",
+    "https://downloads.psl.noaa.gov/Datasets/NARR/Dailies/monolevel/hpbl.2023.nc",
+    "https://github.com/geomarker-io/appc/releases/download/hpms_2020_f12_aadt-2025-07-16/hpms_2020_f12_aadt.gpkg",
+    "https://www.mrlc.gov/downloads/sciweb1/shared/mrlc/data-bundles/Annual_NLCD_FctImp_2022_CU_C1V1.zip",
+    "https://gaftp.epa.gov/air/nei/2020/data_summaries/Facility%20Level%20by%20Pollutant.zip"
+  ) |>
+    url_to_filename() |>
+    expect_identical(
+      c(
+        "373b0d2a--tmmx_2025.nc",
+        "d274c988--tl_2024_us_internationalboundary.zip",
+        "36b8531e--hms_smoke20251225.zip",
+        "afadc45b--hpbl.2023.nc",
+        "4303dd87--hpms_2020_f12_aadt.gpkg",
+        "d84440c1--Annual_NLCD_FctImp_2022_CU_C1V1.zip",
+        "b883536e--Facility%20Level%20by%20Pollutant.zip"
+      )
+    )
+})
+
+test_that("geomarker_download_file works", {
+  the_url <- "https://www2.census.gov/geo/tiger/TIGER2024/INTERNATIONALBOUNDARY/tl_2024_us_internationalboundary.zip"
+  geomarker_download_file(the_url) |>
+    expect_visible() |>
+    expect_identical(
+      file.path(
+        geomarker_data_dir(),
+        "d274c988--tl_2024_us_internationalboundary.zip"
+      )
+    )
+  geomarker_download_file(the_url, quiet = TRUE) |>
+    expect_invisible()
+})

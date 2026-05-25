7df512bcdd960c6ef3a72d2e0492ec3cf6f0a1af
Cole Brokamp <cole.brokamp@gmail.com>
2026-05-24 22:38:06 -0400

    coerce s2_join_tiger_bg to s2_cell

--- tests/testthat/test-tiger.R
+++ tests/testthat/test-tiger.R
@@ -7,8 +7,15 @@ test_that("s2_join_tiger_bg() works", {
     R_GEOMARKER_NO_DOWNLOAD = "true"
   )
   set.seed(12)
-  out <- s2_join_tiger_bg(s2::as_s2_cell(s2cd_example_cincy(n_locations = 16L)))
+  out <- s2_join_tiger_bg(s2cd_example_cincy(n_locations = 16L))
   expect_type(out, "character")
   expect_length(out, 16)
   expect_true(all(sapply(out, nchar) == 12))
+})
+
+test_that("s2_join_tiger_bg() validates full-resolution cells", {
+  expect_error(
+    s2_join_tiger_bg(s2::s2_cell("8841")),
+    "full-resolution level 30"
+  )
 })

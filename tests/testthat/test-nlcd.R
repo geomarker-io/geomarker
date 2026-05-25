4498e6f68c03926e4a2a0a71e9df1b910c6cfcfd
Cole Brokamp <cole.brokamp@gmail.com>
2026-05-24 22:40:24 -0400

    coerce s2 inputs in tiger block group lookup

--- tests/testthat/test-nlcd.R
+++ tests/testthat/test-nlcd.R
@@ -16,14 +16,14 @@ test_that("get_nlcd_fct_imp_data works with fixture dir", {
 })
 
 test_that("get_nlcd_fct_imp_data works with muliple years", {
-  skip("takes forever")
+  skip("nlcd lookups")
   skip_on_cran()
   skip_on_ci()
   skip_if_offline()
   set.seed(99)
   xx <- s2cd_example_cincy(10L, 5L, "poisson+1")
   xx_dates <- s2cd_dates(xx)
-  xx_dates[[2]][1] <- xx_dates[[2]][1] - (365 * 2)
+  xx_dates[[2]][1] <- xx_dates[[2]][1] - (365 * 2) # add 2024 to warn
   xx <- s2cd(s2::as_s2_cell(xx), xx_dates)
   expect_warning(out <- get_nlcd_fct_imp_data(xx, buffer = 400))
   expect_length(out, length(xx))
